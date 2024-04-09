f569c4d8f1e39ee739600dc2a5c459c18ca31789
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-04-09 14:01:50 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -21,6 +21,30 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter_by(id = id).first()
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status =404
+
+    return make_response(body, status)  
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_min_magnitude(magnitude):
+    quakes = []
+    for quake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        quakes.append(quake.to_dict())
+    
+    body = {
+            'count': len(quakes),
+            'quakes': quakes
+        }
+
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
